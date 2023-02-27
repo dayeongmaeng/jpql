19b5c2210074460c7657f0be66f0f44e3716af55
dayeongmaeng <dayeongmaeng@gmail.com>
2023-02-27 16:05:25 +0900

    feat: JPQL 함수

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,29 +19,18 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
-
             Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-
+            member.setUsername("관리자1");
             em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            String query =
-//                    "select " +
-//                            "case when m.age <= 10 then '학생요금'" +
-//                            "     when m.age >= 60 then '경로요금'" +
-//                            "     else '일반요금'" +
-//                            "     end " +
-//                            "from Member m";
-            String query = "select nullif(m.username, '관리자') as username from Member m";
+            String query = "select group_concat(m.username) from Member m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 

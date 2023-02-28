ff23afc6db9fbf632d7abcf584da4c7680c72119
dayeongmaeng <dayeongmaeng@gmail.com>
2023-02-28 13:05:21 +0900

    feat: 경로 표현식

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,5 +1,6 @@
 package jpql;
 
+import java.util.Collections;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -7,6 +8,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import org.hibernate.mapping.Collection;
 
 public class JpaMain {
 
@@ -19,23 +21,23 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
             Member member = new Member();
             member.setUsername("관리자1");
+            member.setTeam(team);
             em.persist(member);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select group_concat(m.username) from Member m";
-            List<String> result = em.createQuery(query, String.class)
-                    .getResultList();
-
-            System.out.println("result = " + result.get(0));
-
+            String query = "select m.username from Team t join t.members m";
+            List<String> result = em.createQuery(query, String.class).getResultList();
             tx.commit();
 
         } catch (Exception e) {
